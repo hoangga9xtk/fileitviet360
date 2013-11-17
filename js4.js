280915956e35d0a701f4fd4e679aa5e1b4c79441
itviet360@gmail.com <itviet360@gmail.com@ddad7e02-5388-82a8-371e-014bb5e4f47a>
2013-11-17 03:24:31 +0000

    JS4

---
+++ js4.js
@@ -0,0 +1,5 @@
+﻿(function() {
+    var po = document.createElement(&#39;script&#39;); po.type = &#39;text/javascript&#39;; po.async = true;
+    po.src = &#39;https://apis.google.com/js/plusone.js&#39;;
+    var s = document.getElementsByTagName(&#39;script&#39;)[0]; s.parentNode.insertBefore(po, s);
+  })();
