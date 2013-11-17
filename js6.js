0dfdfece50fefc9c6e26cea502665ba74ca18584
itviet360@gmail.com <itviet360@gmail.com@ddad7e02-5388-82a8-371e-014bb5e4f47a>
2013-11-17 03:25:24 +0000

    JS 6

---
+++ js6.js
@@ -0,0 +1,9 @@
+﻿  var _gaq = _gaq || [];
+  _gaq.push([&#39;_setAccount&#39;, &#39;UA-37195737-1&#39;]);
+  _gaq.push([&#39;_trackPageview&#39;]);
+
+  (function() {
+    var ga = document.createElement(&#39;script&#39;); ga.type = &#39;text/javascript&#39;; ga.async = true;
+    ga.src = (&#39;https:&#39; == document.location.protocol ? &#39;https://ssl&#39; : &#39;http://www&#39;) + &#39;.google-analytics.com/ga.js&#39;;
+    var s = document.getElementsByTagName(&#39;script&#39;)[0]; s.parentNode.insertBefore(ga, s);
+  })();
