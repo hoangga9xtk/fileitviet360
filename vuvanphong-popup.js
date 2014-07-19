bdaa1dcb77184f43654eac34dbe1b13819076e5c
itviet360@gmail.com <itviet360@gmail.com@ddad7e02-5388-82a8-371e-014bb5e4f47a>
2014-07-19 04:22:07 +0000

    POPUP hien giua trang 1

---
+++ vuvanphong-popup.js
@@ -0,0 +1,427 @@
+/* 
+==============================================================================
+A Premium jQuery popup developed by STCNetwork.org, with five unique features:
+==============================================================================
+1. Cookie
+2. Count Down timer
+3. Falling christmas Snow
+4. 3 Animation Styles
+5. 2 Themes
+
+*/
+
+// Christmas Snow
+
+(function($){
+	$.snowfall = function(element, options){
+		var	defaults = {
+				flakeCount : 35,
+				flakeColor : '#ffffff',
+				flakeIndex: 999999,
+				minSize : 1,
+				maxSize : 2,
+				minSpeed : 1,
+				maxSpeed : 5,
+				round : false,
+				shadow : false,
+				collection : false,
+				collectionHeight : 40,
+				deviceorientation : false
+			},
+			options = $.extend(defaults, options),
+			random = function random(min, max){
+				return Math.round(min + Math.random()*(max-min)); 
+			};
+			
+			$(element).data("snowfall", this);			
+			
+			// Snow flake object
+			function Flake(_x, _y, _size, _speed, _id)
+			{
+				// Flake properties
+				this.id = _id; 
+				this.x  = _x;
+				this.y  = _y;
+				this.size = _size;
+				this.speed = _speed;
+				this.step = 0;
+				this.stepSize = random(1,10) / 100;
+	
+				if(options.collection){
+					this.target = canvasCollection[random(0,canvasCollection.length-1)];
+				}
+				
+				var flakeMarkup = $(document.createElement("div")).attr({'class': 'snowfall-flakes', 'id' : 'flake-' + this.id}).css({'width' : this.size, 'height' : this.size, 'background' : options.flakeColor, 'position' : 'absolute', 'top' : this.y, 'left' : this.x, 'fontSize' : 0, 'zIndex' : options.flakeIndex});
+				
+				if($(element).get(0).tagName === $(document).get(0).tagName){
+					$('body').append(flakeMarkup);
+					element = $('body');
+				}else{
+					$(element).append(flakeMarkup);
+				}
+				
+				this.element = document.getElementById('flake-' + this.id);
+				
+				// Update function, used to update the snow flakes, and checks current snowflake against bounds
+				this.update = function(){
+					this.y += this.speed;
+					
+					if(this.y > (elHeight) - (this.size  + 6)){
+						this.reset();
+					}
+					
+					this.element.style.top = this.y + 'px';
+					this.element.style.left = this.x + 'px';
+					
+					this.step += this.stepSize;
+
+					if (doRatio === false) {
+						this.x += Math.cos(this.step);
+					} else {
+						this.x += (doRatio + Math.cos(this.step));
+					}
+
+					// Pileup check
+					if(options.collection){
+						if(this.x > this.target.x && this.x < this.target.width + this.target.x && this.y > this.target.y && this.y < this.target.height + this.target.y){
+							var ctx = this.target.element.getContext("2d"),
+								curX = this.x - this.target.x,
+								curY = this.y - this.target.y,
+								colData = this.target.colData;
+								
+								if(colData[parseInt(curX)][parseInt(curY+this.speed+this.size)] !== undefined || curY+this.speed+this.size > this.target.height){
+									if(curY+this.speed+this.size > this.target.height){
+										while(curY+this.speed+this.size > this.target.height && this.speed > 0){
+											this.speed *= .5;
+										}
+
+										ctx.fillStyle = "#fff";
+										
+										if(colData[parseInt(curX)][parseInt(curY+this.speed+this.size)] == undefined){
+											colData[parseInt(curX)][parseInt(curY+this.speed+this.size)] = 1;
+											ctx.fillRect(curX, (curY)+this.speed+this.size, this.size, this.size);
+										}else{
+											colData[parseInt(curX)][parseInt(curY+this.speed)] = 1;
+											ctx.fillRect(curX, curY+this.speed, this.size, this.size);
+										}
+										this.reset();
+									}else{
+										// flow to the sides
+										this.speed = 1;
+										this.stepSize = 0;
+									
+										if(parseInt(curX)+1 < this.target.width && colData[parseInt(curX)+1][parseInt(curY)+1] == undefined ){
+											// go left
+											this.x++;
+										}else if(parseInt(curX)-1 > 0 && colData[parseInt(curX)-1][parseInt(curY)+1] == undefined ){
+											// go right
+											this.x--;
+										}else{
+											//stop
+											ctx.fillStyle = "#fff";
+											ctx.fillRect(curX, curY, this.size, this.size);
+											colData[parseInt(curX)][parseInt(curY)] = 1;
+											this.reset();
+										}
+									}
+								}
+						}
+					}
+					
+					if(this.x > (elWidth) - widthOffset || this.x < widthOffset){
+						this.reset();
+					}
+				}
+				
+				// Resets the snowflake once it reaches one of the bounds set
+				this.reset = function(){
+					this.y = 0;
+					this.x = random(widthOffset, elWidth - widthOffset);
+					this.stepSize = random(1,10) / 100;
+					this.size = random((options.minSize * 100), (options.maxSize * 100)) / 100;
+					this.speed = random(options.minSpeed, options.maxSpeed);
+				}
+			}
+		
+			// Private vars
+			var flakes = [],
+				flakeId = 0,
+				i = 0,
+				elHeight = $(element).height(),
+				elWidth = $(element).width(),
+				widthOffset = 0,
+				snowTimeout = 0;
+		
+			// Collection Piece ******************************
+			if(options.collection !== false){
+				var testElem = document.createElement('canvas');
+				if(!!(testElem.getContext && testElem.getContext('2d'))){
+					var canvasCollection = [],
+						elements = $(options.collection),
+						collectionHeight = options.collectionHeight;
+					
+					for(var i =0; i < elements.length; i++){
+							var bounds = elements[i].getBoundingClientRect(),
+								canvas = document.createElement('canvas'),
+								collisionData = [];
+
+							if(bounds.top-collectionHeight > 0){									
+								document.body.appendChild(canvas);
+								canvas.style.position = 'absolute';
+								canvas.height = collectionHeight;
+								canvas.width = bounds.width;
+								canvas.style.left = bounds.left + 'px';
+								canvas.style.top = bounds.top-collectionHeight + 'px';
+								
+								for(var w = 0; w < bounds.width; w++){
+									collisionData[w] = [];
+								}
+								
+								canvasCollection.push({element :canvas, x : bounds.left, y : bounds.top-collectionHeight, width : bounds.width, height: collectionHeight, colData : collisionData});
+							}
+					}
+				}else{
+					// Canvas element isnt supported
+					options.collection = false;
+				}
+			}
+			// ************************************************
+			
+			// This will reduce the horizontal scroll bar from displaying, when the effect is applied to the whole page
+			if($(element).get(0).tagName === $(document).get(0).tagName){
+				widthOffset = 25;
+			}
+			
+			// Bind the window resize event so we can get the innerHeight again
+			$(window).bind("resize", function(){  
+				elHeight = $(element).height();
+				elWidth = $(element).width();
+			}); 
+			
+
+			// initialize the flakes
+			for(i = 0; i < options.flakeCount; i+=1){
+				flakeId = flakes.length;
+				flakes.push(new Flake(random(widthOffset,elWidth - widthOffset), random(0, elHeight), random((options.minSize * 100), (options.maxSize * 100)) / 100, random(options.minSpeed, options.maxSpeed), flakeId));
+			}
+
+			// This adds the style to make the snowflakes round via border radius property 
+			if(options.round){
+				$('.snowfall-flakes').css({'-moz-border-radius' : options.maxSize, '-webkit-border-radius' : options.maxSize, 'border-radius' : options.maxSize});
+			}
+			
+			// This adds shadows just below the snowflake so they pop a bit on lighter colored web pages
+			if(options.shadow){
+				$('.snowfall-flakes').css({'-moz-box-shadow' : '1px 1px 1px #555', '-webkit-box-shadow' : '1px 1px 1px #555', 'box-shadow' : '1px 1px 1px #555'});
+			}
+
+			// On newer Macbooks Snowflakes will fall based on deviceorientation
+			var doRatio = false;
+			if (options.deviceorientation) {
+				$(window).bind('deviceorientation', function(event) {
+					doRatio = event.originalEvent.gamma * 0.1;
+				});
+			}
+
+			// this controls flow of the updating snow
+			function snow(){
+				for( i = 0; i < flakes.length; i += 1){
+					flakes[i].update();
+				}
+				
+				snowTimeout = setTimeout(function(){snow()}, 30);
+			}
+			
+			snow();
+		
+		// Public Methods
+		
+		// clears the snowflakes
+		this.clear = function(){
+						$(element).children('.snowfall-flakes').remove();
+						flakes = [];
+						clearTimeout(snowTimeout);
+					};
+	};
+	
+	// Initialize the options and the plugin
+	$.fn.snowfall = function(options){
+		if(typeof(options) == "object" || options == undefined){		
+				 return this.each(function(i){
+					(new $.snowfall(this, options)); 
+				});	
+		}else if (typeof(options) == "string") {
+			return this.each(function(i){
+				var snow = $(this).data('snowfall');
+				if(snow){
+					snow.clear();
+				}
+			});
+		}
+	};
+})(jQuery);
+
+//Reveal Popup
+
+/*
+ * jQuery Reveal Plugin 1.0
+ * www.ZURB.com
+ * Copyright 2010, ZURB
+ * Free to use under the MIT license.
+ * http://www.opensource.org/licenses/mit-license.php
+*/
+
+
+(function($) {
+
+/*---------------------------
+ Defaults for Reveal
+----------------------------*/
+	 
+/*---------------------------
+ Listener for data-reveal-id attributes
+----------------------------*/
+
+
+
+	$('a[data-reveal-id]').live('click', function(e) {
+		e.preventDefault();
+		var modalLocation = $(this).attr('data-reveal-id');
+		$('#'+modalLocation).reveal($(this).data());
+	});
+
+/*---------------------------
+ Extend and Execute
+----------------------------*/
+
+    $.fn.reveal = function(options) {
+        
+        
+        var defaults = {  
+	    	animation: 'fadeAndPop', //fade, fadeAndPop, none
+		    animationspeed: 300, //how fast animtions are
+		    closeonbackgroundclick: true, //if you click background will modal close?
+		    dismissmodalclass: 'close-reveal-modal' //the class of a button or element that will close an open modal
+    	}; 
+    	
+        //Extend dem' options
+        var options = $.extend({}, defaults, options); 
+	
+        return this.each(function() {
+        
+/*---------------------------
+ Global Variables
+----------------------------*/
+        	var modal = $(this),
+        		topMeasure  = parseInt(modal.css('top')),
+				topOffset = modal.height() + topMeasure,
+          		locked = false,
+				modalBG = $('.reveal-modal-bg');
+
+/*---------------------------
+ Create Modal BG
+----------------------------*/
+			if(modalBG.length == 0) {
+				modalBG = $('<div class="reveal-modal-bg" />').insertAfter(modal);
+			}		    
+     
+/*---------------------------
+ Open & Close Animations
+----------------------------*/
+			//Entrance Animations
+			modal.bind('reveal:open', function () {
+			  modalBG.unbind('click.modalEvent');
+				$('.' + options.dismissmodalclass).unbind('click.modalEvent');
+				if(!locked) {
+					lockModal();
+					if(options.animation == "fadeAndPop") {
+						modal.css({'top': $(document).scrollTop()-topOffset, 'opacity' : 0, 'visibility' : 'visible'});
+						modalBG.fadeIn(options.animationspeed/2);
+						modal.delay(options.animationspeed/2).animate({
+							"top": $(document).scrollTop()+topMeasure + 'px',
+							"opacity" : 1
+						}, options.animationspeed,unlockModal());					
+					}
+					if(options.animation == "fade") {
+						modal.css({'opacity' : 0, 'visibility' : 'visible', 'top': $(document).scrollTop()+topMeasure});
+						modalBG.fadeIn(options.animationspeed/2);
+						modal.delay(options.animationspeed/2).animate({
+							"opacity" : 1
+						}, options.animationspeed,unlockModal());					
+					} 
+					if(options.animation == "none") {
+						modal.css({'visibility' : 'visible', 'top':$(document).scrollTop()+topMeasure});
+						modalBG.css({"display":"block"});	
+						unlockModal()				
+					}
+				}
+				modal.unbind('reveal:open');
+			}); 	
+
+			//Closing Animation
+			modal.bind('reveal:close', function () {
+			  if(!locked) {
+					lockModal();
+					if(options.animation == "fadeAndPop") {
+						modalBG.delay(options.animationspeed).fadeOut(options.animationspeed);
+						modal.animate({
+							"top":  $(document).scrollTop()-topOffset + 'px',
+							"opacity" : 0
+						}, options.animationspeed/2, function() {
+							modal.css({'top':topMeasure, 'opacity' : 1, 'visibility' : 'hidden'});
+							unlockModal();
+						});					
+					}  	
+					if(options.animation == "fade") {
+						modalBG.delay(options.animationspeed).fadeOut(options.animationspeed);
+						modal.animate({
+							"opacity" : 0
+						}, options.animationspeed, function() {
+							modal.css({'opacity' : 1, 'visibility' : 'hidden', 'top' : topMeasure});
+							unlockModal();
+						});					
+					}  	
+					if(options.animation == "none") {
+						modal.css({'visibility' : 'hidden', 'top' : topMeasure});
+						modalBG.css({'display' : 'none'});	
+					}		
+				}
+				modal.unbind('reveal:close');
+			});     
+   	
+/*---------------------------
+ Open and add Closing Listeners
+----------------------------*/
+        	//Open Modal Immediately
+    	modal.trigger('reveal:open')
+			
+			//Close Modal Listeners
+			var closeButton = $('.' + options.dismissmodalclass).bind('click.modalEvent', function () {
+			  modal.trigger('reveal:close')
+			});
+			
+			if(options.closeonbackgroundclick) {
+				modalBG.css({"cursor":"pointer"})
+				modalBG.bind('click.modalEvent', function () {
+				  modal.trigger('reveal:close')
+				});
+			}
+			$('body').keyup(function(e) {
+        		if(e.which===27){ modal.trigger('reveal:close'); } // 27 is the keycode for the Escape key
+			});
+			
+			
+/*---------------------------
+ Animations Locks
+----------------------------*/
+			function unlockModal() { 
+				locked = false;
+			}
+			function lockModal() {
+				locked = true;
+			}	
+			
+        });//each call
+    }//orbit plugin call
+})(jQuery);
+        
